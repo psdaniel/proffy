bb8e6ad91ffa76d99bb0830d909a7ac29ee7a4a7
Daniel <daniel.n10@hotmail.com>
2020-08-07 00:49:57 -0300

    reorganização das pastas e estruturação do backend com nodejs

---
+++ public/scripts/addField.js
@@ -0,0 +1,25 @@
+// Procurar o botão
+document.querySelector("#add-time")
+// Quando clicar no botão
+.addEventListener('click', cloneField)
+
+// Executar uma ação
+function cloneField() {
+    // Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    
+    // pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll('input')
+    
+    // para cada campo, limpar
+    
+    fields.forEach(function(field) {
+        // pegar o field do momento e limpa ele
+        field.value = ""
+    })
+
+    
+    // Colocar na página: onde??
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+    
+}
